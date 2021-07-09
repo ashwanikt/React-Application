57efd1cfd2f4b6b793839c890c31e2b614dd7bd0
ashwanikt <ashwanikt@yahoo.com>
2021-07-09 23:42:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+/* import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+ */
+
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, 
+} from 'react-router-dom'; 
+import HomeComponent from './components/HomeComponent';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="*" component={HomeComponent} /> 
+      </Switch>
+    </Router>
+  )
+}
+export default App;
